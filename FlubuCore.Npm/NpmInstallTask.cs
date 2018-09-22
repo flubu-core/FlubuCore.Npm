37cb7a3ae3a63ae7633bcded8e0dd5f3e49232b7
markoz <mzorec@gmail.com>
2018-09-23 01:57:58 +0200

    Adds documentation.

--- FlubuCore.Npm/NpmInstallTask.cs
+++ FlubuCore.Npm/NpmInstallTask.cs
@@ -29,69 +29,134 @@ namespace FlubuCore.Npm
             return this;
         }
 
+        /// <summary>
+        /// argument will force npm to fetch remote resources even if a local copy exists on disk.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask Force()
         {
             WithArguments("--force");
             return this;
         }
 
+        /// <summary>
+        ///  argument will cause npm to install the package globally rather than locally. 
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask Global()
         {
             WithArguments("--global");
             return this;
         }
 
+        /// <summary>
+        /// argument will cause npm to install the package into your local node_modules folder with the same layout it uses with the global node_modules folder.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask GlobalStyle()
         {
             WithArguments("--global-style");
             return this;
         }
 
+        /// <summary>
+        /// argument will cause npm to not execute any scripts defined in the package.json.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask IgnoreScripts()
         {
             WithArguments("--ignore-scripts");
             return this;
         }
 
+        /// <summary>
+        /// rgument will cause npm to install the package such that versions of npm prior to 1.4, such as the one included with node 0.8, can install the package.
+        /// This eliminates all automatic deduping.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask LegacyBundling()
         {
             WithArguments("--legacy-bundling");
             return this;
         }
 
+        /// <summary>
+        /// argument will cause npm to link global installs into the local space in some cases.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask Link()
         {
             WithArguments("--link");
             return this;
         }
 
+        /// <summary>
+        ///  argument will prevent npm from creating symlinks for any binaries the package might contain.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask NoBinLinks()
         {
             WithArguments("--no-bin-links");
             return this;
         }
 
+        /// <summary>
+        /// argument will prevent optional dependencies from being installed.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask NoOptional()
         {
             WithArguments("--no-optional");
             return this;
         }
 
+        /// <summary>
+        /// argument, which will ignore an available package lock or shrinkwrap file and use the package.json instead.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask NoShrinkWrap()
         {
             WithArguments("--no-shrinkwrap");
             return this;
         }
 
+        public NpmInstallTask NoPackageLock()
+        {
+            WithArguments("--no-package-lock");
+            return this;
+        }
+
+        /// <summary>
+        /// argument will allow npm to find the node source code so that npm can compile native modules.
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns></returns>
         public NpmInstallTask NodeDir(string directory)
         {
             WithArguments($"nodedir={directory}");
             return this;
         }
 
+        /// <summary>
+        /// argument will cause either only devDependencies or only non-devDependencies to be installed regardless of the NODE_ENV.
+        /// </summary>
+        /// <param name="only"></param>
+        /// <returns></returns>
         public NpmInstallTask Only(string only)
         {
             WithArguments($"only={only}");
+            return this;
+        }
+
+        public NpmInstallTask NoAudit()
+        {
+            WithArguments("--no-audit");
+            return this;
+        }
+
+        public NpmInstallTask DryRun()
+        {
+            WithArguments("--dry-run");
             return this;
         }
 
