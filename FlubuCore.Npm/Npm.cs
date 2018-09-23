630545f1ca0dde2baaebea49bac68a71861fb841
markoz <mzorec@gmail.com>
2018-09-23 02:15:28 +0200

    Run script task.

--- FlubuCore.Npm/Npm.cs
+++ FlubuCore.Npm/Npm.cs
@@ -6,9 +6,23 @@ namespace FlubuCore.Npm
 {
     public class Npm
     {
+        /// <summary>
+        /// This command installs a package, and any packages that it depends on.
+        /// If the package has a package-lock or shrinkwrap file, the installation of dependencies will be driven by that, with an npm-shrinkwrap.json taking precedence if both files exist.
+        /// </summary>
+        /// <returns></returns>
         public NpmInstallTask Install()
         {
             return new NpmInstallTask();
+        }
+
+        /// <summary>
+        /// Runs an arbitrary command from a package's "scripts".
+        /// </summary>
+        /// <param name="command"></param>
+        public NpmRunScriptTask RunScript(string command)
+        {
+            return new NpmRunScriptTask(command);
         }
     }
 }
---
+++ FlubuCore.Npm/NpmRunScriptTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlubuCore.Tasks.Process;
+
+namespace FlubuCore.Npm
+{
+    public class NpmRunScriptTask : ExternalProcessTaskBase<NpmRunScriptTask>
+    {
+        protected override string Description { get; set; }
+
+        /// <summary>
+        /// Runs an arbitrary command from a package's "scripts".
+        /// </summary>
+        /// <param name="command"></param>
+        public NpmRunScriptTask(string command)
+        {
+            WithArguments("run-script");
+            WithArguments(command);
+        }
+    }
+}
